fc8b70a9706e4dfa40a3a6e03c7d1ee96864ee4d
frank bahirwa <frankbahirwa@gmail.com>
2024-10-16 10:09:20 +0200

    adding basic api consumes for testing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 const App = ()=>{
+  const [users , setUsers] = useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:8000/users/')
+    .then((response)=>{
+      setUsers(response.data)
+    })
+  } ,[])
+  
+
 return(
   <>
   <p className="text-blue-500">frank akunda mama we </p>
+  <p>{
+    users.map((user , index)=>{
+      return <p key={index}>{user.name}</p>
+    })
+    }</p>
   </>
 )
 }
 
 export default App
+
+// this is the basic sample to fetch the api is from the backend : by frank
